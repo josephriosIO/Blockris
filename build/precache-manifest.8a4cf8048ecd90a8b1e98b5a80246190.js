9360ada455ed7030740e5cdf523bd392e8feb972
josephriosio <blankworldwide@gmail.com>
2019-08-13 16:02:17 -0700

    build file

---
+++ build/precache-manifest.8a4cf8048ecd90a8b1e98b5a80246190.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66e8c373309f5584c5d4ed6deed2df47",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0f20a7a1381fc4f22604",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "9c0594477b39089c2630",
+    "url": "/static/css/main.4078ad24.chunk.css"
+  },
+  {
+    "revision": "0f20a7a1381fc4f22604",
+    "url": "/static/js/2.37302dbf.chunk.js"
+  },
+  {
+    "revision": "9c0594477b39089c2630",
+    "url": "/static/js/main.dd97e011.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "2810fe6b5435e75b3dad63e341b97d02",
+    "url": "/static/media/bg.2810fe6b.png"
+  }
+]);
